b721c9fc85c4c59689b5807662c528fdf4c18c78
newTechUser <116643451+newTechUser@users.noreply.github.com>
2023-10-22 14:07:23 +0400

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const multerStorage = multer.diskStorage({
 });
 
 const upload = multer({ storage: multerStorage });
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
